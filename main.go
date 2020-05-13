398c421686e7be03c2f113856eb425e82a9c34e3
lpf <1198150498@qq.com>
2020-05-13 17:02:06 +0800

    编译Linux二进制文件

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+//filepath: 要编译的文件的路径
+func build(filepath string){
+	_ = os.Setenv("CGO_ENABLED", "0")
+	_ = os.Setenv("GOARCH", "amd64")
+	_ = os.Setenv("GOOS", "linux")
+
+	arg := []string{"build", filepath}
+	if err := exec.Command("go", arg...).Run(); err!=nil {
+		fmt.Println("编译失败:", err)
+	} else{
+		fmt.Println("编译成功")
+	}
+}
+
+func main() {
+	build(`./gin_practice/main.go`)
+}
